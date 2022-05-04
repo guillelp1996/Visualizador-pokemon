435b96b6637f3de191e333a8ca50676b148b8412
Guillermo A. Valdez Saavedra <guillermovaldez8@gmail.com>
2022-05-04 09:59:08 +0200

    ç

--- pokemon.js
+++ pokemon.js
@@ -6,6 +6,12 @@ const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=1126";
 
 fetch(urlApi)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    // console.log(data.results[1].name)
+
+    for(let i = 0; i>data.results.lenght; i++){
+      console.log(data.results[1].name)
+    }
+  });
 
 
